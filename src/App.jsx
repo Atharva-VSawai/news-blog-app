df68779d7763e9ed0d97f8b2847304c435d654b3
Atharva_Sawai <atharva.s.official@gmail.com>
2025-02-21 09:43:32 +0530

    Added Search News Functionality

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import News from './Components/News'
+import './index.css';
+
+
+
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='news-blog-app'>
+        <News />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/News.jsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useState } from 'react'
+import Weather from './Weather'
+import Calender from './Calender'
+import './News.css'
+import userImg from '../assets/images/user.jpg'
+import noImg from '../assets/images/no-img.png'
+import axios from 'axios'
+
+const categories = [
+  'general',
+  'world',
+  'business',
+  'technology',
+  'entertainment',
+  'sports',
+  'health',
+  'science',
+  'business',
+]
+
+const News = () => {
+  const [headline, setHeadline] = useState(null)
+  const [news, setNews] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState('general')
+  const [searchInput, setSearchInput] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      let url = 
+      if(searchQuery) {
+        url = 
+      }
+
+      
+      const response = await axios.get(url)
+      const fetchedNews = response.data.articles
+
+      fetchedNews.forEach((article) => {
+        if (!article.image) {
+          article.image = noImg
+        }
+      })
+
+      setHeadline(fetchedNews[0])
+      setNews(fetchedNews.slice(1, 7))
+
+      console.log(news)
+    }
+    fetchNews()
+  },[selectedCategory, searchQuery])
+
+  const handleCategoryClick = (e,category) => {
+    e.preventDefault();
+    setSearchQuery(searchInput)
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery(searchInput)
+    setSearchInput('')
+  }
+
+  return (
+    <div className='news'>
+      <header className='news-header'>
+        <h1 className='logo'>News & Blogs</h1>
+        <div className='search-bar'>
+          <form onSubmit={handleSearch}>
+            <input type="text" placeholder='Search News...' value={searchInput} onChange={(e)=> setSearchInput(e.target.value)}/>
+            <button type="submit">
+              <i className='fa-solid fa-magnifying-glass'></i>
+            </button>
+          </form>
+
+        </div>
+      </header>
+      <div className='news-content'>
+        <div className='navbar'>
+          <div className='user'>
+            <img src={userImg} alt="User Image" />
+            <p>Marrry's Blog</p>
+          </div>
+          <nav className='categories'>
+            <h1 className="nav-heading">Categories</h1>
+            <div className="nav-links">
+              {categories.map((category) => (
+                <a href='#'
+                   key={category}
+                   className='nav-link'
+                   onClick={(e) => handleCategoryClick(e, category)} >
+                  {category}
+                </a>
+              ))}
+              <a href='#' className='nav-link'>
+                Bookmarks <i className="fa-regular fa-bookmark"></i>
+              </a>
+
+            </div>
+          </nav>
+        </div>
+        <div className='news-section'>
+          {headline && (
+            <div className='headline'>
+              <img src={headline.image || noImg} alt={headline.title}></img>
+              <h2 className="headline-title">
+                {headline.title}
+                <i className="fa-regular fa-bookmark"></i>
+              </h2>
+            </div>)}
+
+
+          <div className='news-grid'>
+            {news.map((article, index) => (
+              <div key = {index} className="news-grid-item">
+                <img src={article.image || noImg} alt={article.title} />
+                <h3>
+                  {article.title}
+                  <i className="fa-regular fa-bookmark"></i>
+                </h3>
+              </div>
+            ))} 
+            </div>
+        </div>
+        <div className='my-blogs'>My Blogs</div>
+        <div className='weather-calendar'>
+          <Weather />
+          <Calender />
+        </div>
+      </div>
+      <footer className='news-footer'>Footer</footer>
+    </div>
+  )
+}
+
+export default News
