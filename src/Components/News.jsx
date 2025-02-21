01207e13096c13f50f3fbf215a37b5c4e968f7b3
Atharva_Sawai <atharva.s.official@gmail.com>
2025-02-21 19:29:53 +0530

    Added Book Mark Fynctionality and Read MOre button redirect to main news

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -5,6 +5,8 @@ import './News.css'
 import userImg from '../assets/images/user.jpg'
 import noImg from '../assets/images/no-img.png'
 import axios from 'axios'
+import NewsModal from './NewsModal'
+import Bookmarks from './Bookmarks'
 
 const categories = [
   'general',
@@ -24,15 +26,19 @@ const News = () => {
   const [selectedCategory, setSelectedCategory] = useState('general')
   const [searchInput, setSearchInput] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
+  const [showModal, setShowModal] = useState(false)
+  const [selectedArticle, setSelectedArticle] = useState(null)
+  const [bookmarks, setBookmarks] = useState([])
+  const [showBookmarksModal, setShowBookmarksModal] = useState(false)
 
   useEffect(() => {
     const fetchNews = async () => {
-      let url = 
-      if(searchQuery) {
-        url = 
+      //let url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=9b4bac26e28042f83f64dd1abfd10e18`;
+      if (searchQuery) {
+       //  url = `https://gnews.io/api/v4/search?q=${searchQuery}&lang=en&apikey=9b4bac26e28042f83f64dd1abfd10e18`
       }
 
-      
+
       const response = await axios.get(url)
       const fetchedNews = response.data.articles
 
@@ -48,9 +54,9 @@ const News = () => {
       console.log(news)
     }
     fetchNews()
-  },[selectedCategory, searchQuery])
+  }, [selectedCategory, searchQuery])
 
-  const handleCategoryClick = (e,category) => {
+  const handleCategoryClick = (e, category) => {
     e.preventDefault();
     setSearchQuery(searchInput)
   }
@@ -61,13 +67,29 @@ const News = () => {
     setSearchInput('')
   }
 
+  const handleArticleClick = (article) => {
+    setSelectedArticle(article)
+    setShowModal(true)
+
+  }
+
+  const handleBookmarkClick = (article) => {
+    setBookmarks((prevBookmarks) => {
+      const updateBookmarks = prevBookmarks.find
+        ((bookmark) => bookmark.title === article.title) ?
+        prevBookmarks.filter((bookmark) => bookmark.title !== article.title) :
+        [...prevBookmarks, article]
+      return updateBookmarks
+    })
+  }
+
   return (
     <div className='news'>
       <header className='news-header'>
         <h1 className='logo'>News & Blogs</h1>
         <div className='search-bar'>
           <form onSubmit={handleSearch}>
-            <input type="text" placeholder='Search News...' value={searchInput} onChange={(e)=> setSearchInput(e.target.value)}/>
+            <input type="text" placeholder='Search News...' value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
             <button type="submit">
               <i className='fa-solid fa-magnifying-glass'></i>
             </button>
@@ -86,14 +108,14 @@ const News = () => {
             <div className="nav-links">
               {categories.map((category) => (
                 <a href='#'
-                   key={category}
-                   className='nav-link'
-                   onClick={(e) => handleCategoryClick(e, category)} >
+                  key={category}
+                  className='nav-link'
+                  onClick={(e) => handleCategoryClick(e, category)} >
                   {category}
                 </a>
               ))}
-              <a href='#' className='nav-link'>
-                Bookmarks <i className="fa-regular fa-bookmark"></i>
+              <a href='#' className='nav-link' onClick={() => setShowBookmarksModal(true)}>
+                Bookmarks <i className="fa-solid fa-bookmark"></i>
               </a>
 
             </div>
@@ -101,27 +123,53 @@ const News = () => {
         </div>
         <div className='news-section'>
           {headline && (
-            <div className='headline'>
+            <div className='headline' onClick={() => handleArticleClick(headline)}>
               <img src={headline.image || noImg} alt={headline.title}></img>
               <h2 className="headline-title">
                 {headline.title}
-                <i className="fa-regular fa-bookmark"></i>
+                <i className={`${bookmarks.some((bookmark) => bookmark.title === headline.title)
+                  ? "fa-solid"
+                  : "fa-regular"
+                  } fa-bookmark bookmark`}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleBookmarkClick(headline);
+                  }}>
+                </i>
               </h2>
             </div>)}
 
 
           <div className='news-grid'>
             {news.map((article, index) => (
-              <div key = {index} className="news-grid-item">
+              <div key={index} className="news-grid-item" onClick={() => handleArticleClick(article)}>
                 <img src={article.image || noImg} alt={article.title} />
                 <h3>
                   {article.title}
-                  <i className="fa-regular fa-bookmark"></i>
+                  <i className={`${bookmarks.some((bookmark) => bookmark.title === article.title)
+                    ? "fa-solid"
+                    : "fa-regular"
+                    } fa-bookmark bookmark`}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleBookmarkClick(article);
+                    }}>
+                  </i>
+
+
                 </h3>
               </div>
-            ))} 
-            </div>
+            ))}
+          </div>
         </div>
+        <NewsModal show={showModal} article={selectedArticle} onClose={() => setShowModal(false)} />
+        <Bookmarks
+          show={showBookmarksModal}
+          bookmarks={bookmarks}
+          onClose={() => setShowBookmarksModal(false)}
+          onSelectArticle={handleArticleClick}
+          onDeleteBookmark={handleBookmarkClick}
+        />
         <div className='my-blogs'>My Blogs</div>
         <div className='weather-calendar'>
           <Weather />
