e84c7acaf99419825be71f3715bb2f215a36539f
Atharva_Sawai <atharva.s.official@gmail.com>
2025-02-21 22:36:22 +0530

    Customized Weather Gadget by applying css in weather.css

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -33,9 +33,9 @@ const News = () => {
 
   useEffect(() => {
     const fetchNews = async () => {
-      let url = 
+      //let url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey=9d414c9c34cf5415df577d308b96aeeb`
       if(searchQuery) {
-        url =  
+        //url =  `https://gnews.io/api/v4/search?q=${searchQuery}&lang=en&apikey=9d414c9c34cf5415df577d308b96aeeb`
 
       }
              
