02edecd3d3d618da8d70405b844b0a37977899af
Atharva_Sawai <atharva.s.official@gmail.com>
2025-02-23 21:20:15 +0530

    Created and Designed My Blogs Section

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -4,6 +4,10 @@ import Calender from './Calender'
 import './News.css'
 import userImg from '../assets/images/user.jpg'
 import noImg from '../assets/images/no-img.png'
+import blogImg1 from '../assets/images/blog1.jpg'
+import blogImg2 from '../assets/images/blog2.jpg'
+import blogImg3 from '../assets/images/blog3.jpg'
+import blogImg4 from '../assets/images/blog4.jpg'
 import axios from 'axios'
 import NewsModal from './NewsModal'
 import Bookmarks from './Bookmarks'
@@ -33,9 +37,9 @@ const News = ({onShowBlogs}) => {
 
   useEffect(() => {
     const fetchNews = async () => {
-      let url = 
+      let url = `https://gnews.io/api/v4/top-headlines?category=${selectedCategory}&lang=en&apikey={}}`
       if(searchQuery) {
-        url = 
+        url =  `https://gnews.io/api/v4/search?q=${searchQuery}&lang=en&apikey={}}`
 
       }
              
@@ -176,7 +180,62 @@ const News = ({onShowBlogs}) => {
           onSelectArticle={handleArticleClick}
           onDeleteBookmark={handleBookmarkClick}
         />
-        <div className='my-blogs'>My Blogs</div>
+        <div className='my-blogs'>
+            <h1 className="my-blogs-heading">My Blogs</h1>
+            <div className="blog-posts">
+              <div className="blog-post">
+                <img src={blogImg1} alt="" />
+                <h3>Lorem ipsum dolor sit.</h3>
+                <div className="post-buttons">
+                  <button className="edit-post">
+                    <i className="bx bxs-edit"></i>
+                  </button>
+                  <button className="delete-post">
+                    <i className="bx bxs-x-circle"></i>
+                  </button>
+                </div>
+              </div>
+
+              <div className="blog-post">
+                <img src={blogImg2} alt="" />
+                <h3>Lorem ipsum dolor sit.</h3>
+                <div className="post-buttons">
+                  <button className="edit-post">
+                    <i className="bx bxs-edit"></i>
+                  </button>
+                  <button className="delete-post">
+                    <i className="bx bxs-x-circle"></i>
+                  </button>
+                </div>
+              </div>
+
+              <div className="blog-post">
+                <img src={blogImg3} alt="" />
+                <h3>Lorem ipsum dolor sit.</h3>
+                <div className="post-buttons">
+                  <button className="edit-post">
+                    <i className="bx bxs-edit"></i>
+                  </button>
+                  <button className="delete-post">
+                    <i className="bx bxs-x-circle"></i>
+                  </button>
+                </div>
+              </div>
+
+              <div className="blog-post">
+                <img src={blogImg4} alt="" />
+                <h3>Lorem ipsum dolor sit.</h3>
+                <div className="post-buttons">
+                  <button className="edit-post">
+                    <i className="bx bxs-edit"></i>
+                  </button>
+                  <button className="delete-post">
+                    <i className="bx bxs-x-circle"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+        </div>
         <div className='weather-calendar'>
           <Weather />
           <Calender />
